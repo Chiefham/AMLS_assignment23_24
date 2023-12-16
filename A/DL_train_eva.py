609dd3994d6461f9caf21b5406610f85e84e08ed
WooChickham <1580103639@qq.com>
2023-12-16 17:41:53 +0000

    LeNet5 Model

--- A/DL_train_eva.py
+++ A/DL_train_eva.py
@@ -5,83 +5,87 @@ from keras.models import Model, load_model, Sequential
 from keras.layers import Dense, Flatten, Dropout
 import tensorflow as tf
 from keras.utils import to_categorical
+from data_loader import data_loader
+import tensorflow as tf
+from keras.layers import Dense, Conv2D, AveragePooling2D, Flatten
+from keras.models import Sequential
+from keras.utils import to_categorical
+from sklearn.metrics import classification_report, cohen_kappa_score
+import numpy as np
+from keras.callbacks import EarlyStopping
+from keras.applications.resnet import ResNet50
 
 
-class DL:
-    def __init__(self, x_train, x_val, x_test, y_train, y_val, y_test):
-        self.x_train = x_train
-        self.x_val = x_val
-        self.x_test = x_test
-        self.y_train = y_train
-        self.y_val = y_val
-        self.y_test = y_test
+class LeNet5:
+    def __init__(self):
+        self.model = Sequential()
 
 
-    def VGG19(self,learning_rate=0.001,epochs=100,batch_size=32):
-        # Data Pre-Processing
-        x_train,y_train = self.transformer(self.x_train,self.y_train)
-        x_val,y_val = self.transformer(self.x_val,self.y_val)
-        x_test,y_test = self.transformer(self.x_test,self.y_test)
-
-
-        # Model Training
-        Model = VGG19(weights="imagenet", include_top=False,input_shape=(28,28,1))
-        for layer in Model.layers[:-1]:
-            layer.trainable = False
-        top_model = Sequential()  # construct the top network
-        top_model.add(Flatten(input_shape=Model.output_shape[1:]))
-        top_model.add(Dense(32, activation='relu'))
-        top_model.add(Dropout(0.5))
-        top_model.add(Dense(2, activation='softmax'))  # binary classfication
-
-        model = Model(
-            inputs=Model.input,
-            outputs=top_model(Model.output)
-        )
-
-        model.compile(
-            loss='categorical_crossentropy',
-            optimizer=tf.keras.optimizers.SGD(lr=learning_rate, decay=1e-6, momentum=0.9, nesterov=True),
-            metrics=['accuracy',keras.metrics.Precision(),keras.metrics.Recall(),keras.metrics.F1Score()]
-        )
-        callback = keras.callbacks.EarlyStopping(monitor='accuracy', patience=2)
-
-        history = model.fit(x_train,y_train,
-                            batch_size = batch_size,
-                            epochs = epochs,validation_data = (x_val,y_val),
-                            callbacks = callback)
-
-
-        # Model Evaluation
-        print(history.history)
-
-
-
-
-
-        # Model Save
-        model.save('./VGG19.model')
-
-        pass
-
-    def DenseNet201(self):
-
-        pass
-
-    def InceptionV3(self):
-
-        pass
-
-    def transformer(self,x,y):
+    def data_transformer(self,x,y):
         X = x.reshape(-1,28,28,1)
         X = X/255
         Y = to_categorical(y,num_classes=2)
 
         return X,Y
 
-class LeNet5:
-    def __init__(self):
-        self.model = Sequential()
+    def train_eva_save(self,x_train,y_train,x_val,y_val,x_test,y_test):
+        # Model Structure
+        self.model.add(Conv2D(input_shape=(28,28,1),
+                              filters=6,
+                              kernel_size=(5,5),
+                              strides=1,
+                              activation=keras.activations.tanh
+                              )
+        )
+
+        self.model.add(AveragePooling2D(pool_size=(2,2)))
+
+        self.model.add(Conv2D(filters=6,
+                              kernel_size=(5, 5),
+                              strides=1,
+                              activation=tf.keras.activations.tanh))
+
+        self.model.add(AveragePooling2D(pool_size=(2, 2)))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(120, activation='relu'))
+        self.model.add(Dense(84, activation='relu'))
+        self.model.add(Dense(2, activation='softmax'))
+
+
+
+
+        # Model Training
+        self.model.compile(
+            loss=keras.losses.CategoricalCrossentropy(),
+            optimizer='RMSprop',
+            metrics = ['accuracy'],
+        )
+        early_stopping = EarlyStopping(monitor='loss', patience=3, restore_best_weights=True)
+        self.model.fit(x=x_train,y=y_train,batch_size=32,epochs=50,verbose=1,
+                       callbacks=[early_stopping],
+                       )
+
+        # Model Evaluation
+        y_pred = self.model.predict(x_test)
+        y_pred_classes = np.argmax(y_pred, axis=1)
+        y_true_classes = np.argmax(y_test, axis=1)
+        report = classification_report(y_true_classes, y_pred_classes,digits=6)
+        kappa = cohen_kappa_score(y_true_classes, y_pred_classes)
+
+        print(report)
+
+        print(kappa)
+
+
+        # Model Save
+
+        self.model.save("LeNet5.model")
+
+
+
+
+
 
 
 
