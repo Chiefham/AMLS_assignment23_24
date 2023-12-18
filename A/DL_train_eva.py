2d742d127a293be79954303e8bb26a039858af4c
WooChickham <1580103639@qq.com>
2023-12-18 19:02:37 +0000

    1218

--- A/DL_train_eva.py
+++ A/DL_train_eva.py
@@ -1,17 +1,28 @@
 import keras
 import tensorflow as tf
-from keras.layers import Dense, Conv2D, AveragePooling2D, Flatten
-from keras.models import Sequential
+from keras.layers import AveragePooling2D, Flatten
 from keras.utils import to_categorical
 from sklearn.metrics import classification_report, cohen_kappa_score
 import numpy as np
 from keras.callbacks import EarlyStopping
+from keras import layers
+from keras import models
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.models import Model
+from keras.layers import Input, Conv2D, BatchNormalization, ReLU, DepthwiseConv2D, GlobalAveragePooling2D, Dense
 
 
-class LeNet5:
-    def __init__(self):
-        self.model = Sequential()
 
+
+class DL:
+    def __init__(self,x_train,x_val,x_test,y_train,y_val,y_test):
+        self.x_train = x_train
+        self.x_val = x_val
+        self.x_test = x_test
+        self.y_train = y_train
+        self.y_val = y_val
+        self.y_test = y_test
 
     def data_transformer(self,x,y):
         X = x.reshape(-1,28,28,1)
@@ -20,59 +31,155 @@ class LeNet5:
 
         return X,Y
 
-    def train_eva_save(self,x_train,y_train,x_val,y_val,x_test,y_test):
+    def LeNet5_train_eva_save(self):
+        # Data Pre_Processing
+        x_train,y_train = self.data_transformer(self.x_train,self.y_train)
+        x_val,y_val = self.data_transformer(self.x_val,self.y_val)
+        x_test,y_test = self.data_transformer(self.x_test,self.y_test)
+
         # Model Structure
-        self.model.add(Conv2D(input_shape=(28,28,1),
-                              filters=6,
-                              kernel_size=(5,5),
-                              strides=1,
-                              activation=keras.activations.tanh
-                              )
-        )
-
-        self.model.add(AveragePooling2D(pool_size=(2,2)))
-
-        self.model.add(Conv2D(filters=6,
-                              kernel_size=(5, 5),
-                              strides=1,
-                              activation=tf.keras.activations.tanh))
-
-        self.model.add(AveragePooling2D(pool_size=(2, 2)))
-
-        self.model.add(Flatten())
-        self.model.add(Dense(120, activation='relu'))
-        self.model.add(Dense(84, activation='relu'))
-        self.model.add(Dense(2, activation='softmax'))
-
-
-
+        model = Sequential()
+        model.add(Conv2D(input_shape=(28,28,1),
+                         filters=6,
+                         kernel_size=(5,5),
+                         strides=1,
+                         activation=keras.activations.tanh
+                         )
+                  )
+        model.add(AveragePooling2D(pool_size=(2,2)))
+        model.add(Conv2D(filters=6,
+                         kernel_size=(5, 5),
+                         strides=1,
+                         activation=tf.keras.activations.tanh))
+        model.add(AveragePooling2D(pool_size=(2, 2)))
+        model.add(Flatten())
+        model.add(Dense(120, activation='relu'))
+        model.add(Dense(84, activation='relu'))
+        model.add(Dense(2, activation='softmax'))
 
         # Model Training
-        self.model.compile(
+        model.compile(
             loss=keras.losses.CategoricalCrossentropy(),
             optimizer='RMSprop',
             metrics = ['accuracy'],
         )
         early_stopping = EarlyStopping(monitor='loss', patience=3, restore_best_weights=True)
-        self.model.fit(x=x_train,y=y_train,batch_size=32,epochs=50,verbose=1,
-                       callbacks=[early_stopping],
-                       )
+        model.fit(x_train,y_train,batch_size=32,epochs=50,verbose=1,
+                  callbacks=[early_stopping],
+                  )
 
         # Model Evaluation
-        y_pred = self.model.predict(x_test)
+        y_pred = model.predict(x_test)
         y_pred_classes = np.argmax(y_pred, axis=1)
         y_true_classes = np.argmax(y_test, axis=1)
         report = classification_report(y_true_classes, y_pred_classes,digits=6)
-        kappa = cohen_kappa_score(y_true_classes, y_pred_classes)
-
         print(report)
 
-        print(kappa)
+        # Model Save
+        model.save("LeNet5.model")
 
+    def MobileNetV2_train_eva_save(self):
+        # Data Pre_Processing
+        x_train, y_train = self.data_transformer(self.x_train, self.y_train)
+        x_val, y_val = self.data_transformer(self.x_val, self.y_val)
+        x_test, y_test = self.data_transformer(self.x_test, self.y_test)
+
+        # Model Structure
+        def conv_block(inputs, filters, kernel_size, strides):
+            x = Conv2D(filters, kernel_size, strides=strides, padding='same')(inputs)
+            x = BatchNormalization()(x)
+            x = ReLU()(x)
+            return x
+        def depthwise_conv_block(inputs, pointwise_conv_filters, depth_multiplier=1, strides=1):
+            x = DepthwiseConv2D((3, 3), padding='same', depth_multiplier=depth_multiplier, strides=strides)(inputs)
+            x = BatchNormalization()(x)
+            x = ReLU()(x)
+
+            x = Conv2D(pointwise_conv_filters, (1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = ReLU()(x)
+            return x
+        input_shape = (28,28,1)
+        num_classes = 2
+        inputs = Input(shape=input_shape)
+        x = conv_block(inputs, 32, (3, 3), strides=(2, 2))
+        for _ in range(13):
+            x = depthwise_conv_block(x, 32)
+        x = depthwise_conv_block(x, 64, strides=(2, 2))
+        for _ in range(13):
+            x = depthwise_conv_block(x, 64)
+        x = depthwise_conv_block(x, 128, strides=(2, 2))
+        for _ in range(13):
+            x = depthwise_conv_block(x, 128)
+        x = depthwise_conv_block(x, 256, strides=(2, 2))
+        for _ in range(5):
+            x = depthwise_conv_block(x, 256)
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(num_classes, activation='sigmoid')(x)
+        model = Model(inputs, x, name='MobileNetV2')
+
+        # Model Training
+        model.compile(optimizer=Adam(lr=0.001), loss='binary_crossentropy', metrics=['accuracy'])
+        early_stopping = EarlyStopping(monitor='loss', patience=3, restore_best_weights=True)
+        model.fit(x_train, y_train, epochs=50, validation_data=(x_val, y_val),
+                  callbacks=[early_stopping],
+                  )
+
+        # Model Evaluation
+        y_pred = model.predict(x_test)
+        y_pred_classes = np.argmax(y_pred, axis=1)
+        y_true_classes = np.argmax(y_test, axis=1)
+        report = classification_report(y_true_classes, y_pred_classes, digits=6)
+        print(report)
+
+    def DNN_train_eva_save(self):
+        # Data Pre_Processing
+        x_train, y_train = self.data_transformer(self.x_train, self.y_train)
+        x_val, y_val = self.data_transformer(self.x_val, self.y_val)
+        x_test, y_test = self.data_transformer(self.x_test, self.y_test)
+
+        # Model Structure
+        model = models.Sequential()
+        model.add(Flatten(input_shape=(28,28)))
+        model.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
+        model.add(layers.Dense(2, activation='softmax'))
+
+        # Model Training
+        model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+        early_stopping = EarlyStopping(monitor='loss', patience=3, restore_best_weights=True)
+        history = model.fit(x_train,
+                            y_train,
+                            epochs=50,
+                            batch_size=32,
+                            validation_data=(x_val, y_val),
+                            callbacks=[early_stopping],
+                            )
+
+        # Model Evaluation
+        y_pred = model.predict(x_test)
+        y_pred_classes = np.argmax(y_pred, axis=1)
+        y_true_classes = np.argmax(y_test, axis=1)
+        report = classification_report(y_true_classes, y_pred_classes, digits=6)
+        print(report)
 
         # Model Save
+        model.save("DNN.model")
 
-        self.model.save("LeNet5.model")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- A/demo1.py
+++ A/demo1.py
@@ -2,6 +2,9 @@ from data_loader import data_loader
 import numpy as np
 from ML_train_eva import ML
 import os
+from DL_train_eva import DL
+import tensorflow as tf
+
 
 parent_folder = os.path.abspath(os.path.join(os.getcwd(), ".."))
 data_path = parent_folder + './Datasets/pneumoniamnist.npz'
@@ -9,7 +12,7 @@ data_path = parent_folder + './Datasets/pneumoniamnist.npz'
 
 x_train,x_val,x_test,y_train,y_val,y_test = data_loader(data_path)
 
-ML_model = ML(x_train,x_val,x_test,y_train,y_val,y_test)
+model = DL(x_train,x_val,x_test,y_train,y_val,y_test)
 
-ML_model.KNN()
+model.LeNet5_train_eva_save()
 
